fb1053860411ff726ba7db939e42df3fc5bbfc6f
Zhong_Ming <a18786717618@126.com>
2020-11-08 23:51:23 +0800

    支持泛型

--- test/Queue/test_queue.c
+++ test/Queue/test_queue.c
@@ -11,21 +11,23 @@ int main(void)
 
 static void queue_common_test(void)
 {
-    Queue q = CreateQueue(5);
-    Enqueue('1', q);
-    Enqueue('2', q);
-    Enqueue('3', q);
-    Enqueue('4', q);
-    Enqueue('5', q);
+    Queue q = CreateQueue(5, sizeof(char));
+    char *seq = "012345678";
+
+    Enqueue(seq, sizeof(char), q);
+    Enqueue(seq + 1, sizeof(char), q);
+    Enqueue(seq + 2, sizeof(char), q);
+    Enqueue(seq + 3, sizeof(char), q);
+    Enqueue(seq + 4, sizeof(char), q);
     assert_true("IsFull", IsFull(q));
     
     /* Enqueue, Dequeque and recorded size*/
-    char actual_elem = FrontAndDequeue(q);
-    check_char("Element", '1', actual_elem);
+    char actual_elem = (*(char *)FrontAndDequeue(sizeof(char), q));
+    check_char("Element", '0', actual_elem);
     check_unsigned("Size", 4, q->Size);
-    actual_elem = FrontAndDequeue(q);
-    check_char("Element", '2', actual_elem);
-    Enqueue('6', q);
+    actual_elem = (*(char *)FrontAndDequeue(sizeof(char), q));
+    check_char("Element", '1', actual_elem);
+    Enqueue(seq + 5, sizeof(char), q);
     check_unsigned("Size", 4, q->Size);
 
     /* If the queue is circular */
@@ -35,13 +37,13 @@ static void queue_common_test(void)
     Dequeue(q);
     Dequeue(q);
     assert_true("IsEmpty", IsEmpty(q));
-    Enqueue('7', q);
-    Enqueue('8', q);
+    Enqueue(seq + 6, sizeof(char), q);
+    Enqueue(seq + 7, sizeof(char), q);
     check_unsigned("Size", 2, q->Size);
-    actual_elem = FrontAndDequeue(q);
+    actual_elem = *((char *)FrontAndDequeue(sizeof(char), q));
+    check_char("Element", '6', actual_elem);
+    actual_elem = *((char *)FrontAndDequeue(sizeof(char), q));
     check_char("Element", '7', actual_elem);
-    actual_elem = FrontAndDequeue(q);
-    check_char("Element", '8', actual_elem);
     assert_true("IsEmpty", IsEmpty(q));
 
     DisposeQueue(q);
