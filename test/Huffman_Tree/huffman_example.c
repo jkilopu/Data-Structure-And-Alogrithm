4a83521d2463821eeaf21ff4f7301cc594b08742
Zhong_Ming <a18786717618@126.com>
2020-11-13 23:42:39 +0800

    部分函数重命名

--- test/Huffman_Tree/huffman_example.c
+++ test/Huffman_Tree/huffman_example.c
@@ -1,35 +1,43 @@
 #include "huffman_tree.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
     Weighted_Char arr[10] = {{'a', 2}, {'b', 9}, {'c', 5}, {'d', 7}, {'e', 8}, {'f', 1}, {'g', 3}, {'h', 4}, {'i', 6}, {'j', 0}};
     H_Tree *h_tree = create_huffman_tree(arr, 10);
+    puts("\nHuffman tree:");
     print_huffman_tree_weighted_char(h_tree);
+    putchar('\n');
 
     puts("\nHuffman code:");
     puts("Format: ch-bits:");
     print_huffman_code(h_tree, 0, 0);
+    putchar('\n');
 
     puts("\nHuffman code in array:");
-    Bit_Array *ba = create_huffman_code(h_tree);
+    Bit_Array *ba_table = create_huffman_code_table(h_tree);
     puts("Format: ch-len-bits:");
     for (int i = 0; i < 10; i++)
     {
-        unsigned int len = ba['a' + i].len;
+        unsigned int len = ba_table['a' + i].len;
         printf("%c-%u-", 'a' + i, len);
-        unsigned int bytes_num = len / 8 + (len % 8 ? 1 : 0);
-        for (int j = 0; j < bytes_num; j++)
-        {
-            unsigned int code = (ba['a' + i].arr)[j];
-            print_bin(code, (j == bytes_num - 1 ? len % 8 : 8));
-            putchar('\n');
-        }
+        print_bit_array(&ba_table['a' + i]);
+        putchar('\n');
     }
     putchar('\n');
 
+    char *seq = "abc";
+    printf("\nHuffman coded sequence: %s\n", seq);
+    Bit_Array *ba = create_huffman_code(seq, strlen(seq), ba_table);
+    for (unsigned int i = 0; i < 3; i++)
+        print_bit_array(&ba[i]);
+    putchar('\n');
+
+    free(ba);
     destroy_huffman_tree(h_tree);
-    destroy_huffman_code(ba);
-    puts("Done.");
+    destroy_huffman_code_table(ba_table);
+    puts("\nDone.");
     return 0;
 }
