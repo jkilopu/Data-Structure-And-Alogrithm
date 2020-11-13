09e7557fbb33a5eef26ac042b7aef5e3966770a2
Zhong_Ming <a18786717618@126.com>
2020-11-13 23:37:17 +0800

    命名修改

--- lib/inc/huffman_tree.h
+++ lib/inc/huffman_tree.h
@@ -28,15 +28,17 @@ typedef struct _bit_array
 typedef H_Node H_Tree;
 
 H_Tree *create_huffman_tree(Weighted_Char weighted_chars[], int num);
-Bit_Array *create_huffman_code(H_Tree *h_tree);
-static void create_huffman_code_recursive_helper(H_Tree *h_tree, unsigned int code, unsigned int len, Bit_Array *ba);
+Bit_Array *create_huffman_code_table(H_Tree *h_tree);
+static void create_huffman_code_table_recursive_helper(H_Tree *h_tree, unsigned int code, unsigned int len, Bit_Array *ba);
 void print_huffman_tree_weighted_char(H_Tree *h_tree);
 void print_huffman_code(H_Tree *h_tree, unsigned int code, unsigned int length);
+Bit_Array *create_huffman_code(char *seq, unsigned int num, Bit_Array *ba_table);
+void print_bit_array(Bit_Array *ba);
 H_Node **create_huffman_nodes(Weighted_Char weighted_chars[], int num);
 void select_2_min(H_Node **nodes, int num, int *min_1, int *min_2);
 void print_bin(unsigned int code, unsigned int length);
 inline static bool is_pow_2(unsigned int a);
-void destroy_huffman_code(Bit_Array *ba);
+void destroy_huffman_code_table(Bit_Array *ba_table);
 void destroy_huffman_tree(H_Tree *h_tree);
 
 #endif
