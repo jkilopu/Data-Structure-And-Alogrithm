3f7265cbfa3fa59bb578a9d3a0ff685a3591acc2
Zhong_Ming <a18786717618@126.com>
2020-11-16 00:20:52 +0800

    1. 图的创建 2. 基本的bfs、dfs

--- lib/inc/graph.h
+++ lib/inc/graph.h
@@ -1,29 +1,48 @@
+/**
+ * 
+ * 图：
+ * 目前的数据结构：邻接表（用到了SList）
+ * 目前构造函数创建的图：无向图
+ * 
+ * TODO:
+ * 1. 图、链表的迭代器？（如何实现？）
+ * 2. 删除图、图的本地构造函数
+ * 3. 更多种图的储存方法选择（逆邻接表、邻接矩阵、十字链表。。。）
+ * 4. 如何区分有向图和无向图，代码有什么区别
+ * 5. 单元测试
+ * 
+ * 小问题：
+ * 1. 邻接表种每个元素都是SList，都有elem_size=sizeof(unsigned int)，有点浪费，该怎么办？（分离出新的struct？）
+ */
 #ifndef _GRAPH_H
 #define _GRAPH_H
 
 #include "slist.h"
+#include <ctype.h>
 
-typedef struct _graph_node {
-    unsigned int pos;
-    struct _graph_node *next;
-} G_Node;
-
-typedef struct _node_head {
-    int data;
-    struct _graph_node *next;
-} Node_Head;
+typedef struct _adjacency_list { // 继承struct _s_list
+    struct _s_list plain_slist; // tail to head
+    void *data;
+} * Adj_List;
 
 typedef struct _graph {
-    Node_Head *adj;
+    Adj_List adj_arr; // 邻接表
+    size_t elem_size;
     unsigned int V, E;
-} Graph;
+} * Graph;
 
 typedef struct _edge {
-    unsigned int tail;
-    unsigned int head;
+    unsigned int arc_tail_idx;
+    unsigned int arc_head_idx;
 } Edge;
 
-Graph *create_empty_graph(const unsigned int V);
-Graph *create_graph_with_nodes(const int data[], const unsigned int V, const Edge es[], const unsigned int E);
+/* 创建一个最大结点数为V，元素大小为elem_size的空的泛型图 */
+Graph create_empty_graph(const unsigned int V, const size_t elem_size);
+/* 创建一个V个结点，E条边的泛型图 */
+Graph create_graph(const void *data_arr, const unsigned int V, const size_t elem_size, const Edge es[], const unsigned int E);
+void dfs_graph(const Graph graph);
+static void dfs_graph_recursive_helper(const Adj_List adj_arr, const unsigned int idx, bool is_tranversed_arr[]);
+void bfs_graph(Graph graph);
+static void bfs_graph_recursive_helper(const Adj_List adj_arr, const unsigned int V, bool is_tranversed_arr[]);
 
 #endif
--- lib/src/graph.c
+++ lib/src/graph.c
@@ -1,27 +1,111 @@
 #include "graph.h"
-#include <stdlib.h>
+#include "queue.h"
+#include "fatal.h"
+#include <string.h>
+#include <stdbool.h>
 
-Graph *create_empty_graph(const unsigned int V)
+Graph create_empty_graph(const unsigned int V, const size_t elem_size)
 {
-    Graph *new_graph = malloc(sizeof(struct _graph));
+    Graph new_graph = malloc_fatal(sizeof(struct _graph), "create_empty_graph - new_graph");
     new_graph->V = V;
     new_graph->E = 0;
-    new_graph->adj = calloc(V, sizeof(struct _node_head));
+    new_graph->adj_arr = calloc_fatal(V, sizeof(struct _adjacency_list), "create_empty_graph - new_graph->adj_arr"); // data成员为空，代表图中没有结点
+    for (unsigned int i = 0; i < V; i++)
+        CreateLocalSList(&new_graph->adj_arr[i].plain_slist, sizeof(unsigned int)); // 邻接表初始化，头指针和尾指针皆为NULL，没有边
+    new_graph->elem_size = elem_size;
     return new_graph;
 }
 
-Graph *create_graph_with_nodes(const int data[], const unsigned int V, const Edge es[], const unsigned int E)
+Graph create_graph(const void *data_arr, const unsigned int V, const size_t elem_size, const Edge es[], const unsigned int E)
 {
-    Graph *new_graph = create_empty_graph(V);
+    Graph new_graph = create_empty_graph(V, elem_size);
     new_graph->E = E;
+    /* 创建结点及其邻接表 */
     for (unsigned int i = 0; i < V; i++)
-        new_graph->adj[i].data = data[i];
+    {
+        new_graph->adj_arr[i].data = malloc_fatal(sizeof(new_graph->elem_size), "create_graph_with_nodes");
+        void *src_addr = (unsigned char *)data_arr + elem_size * i; // copy
+        memcpy(new_graph->adj_arr[i].data, src_addr, new_graph->elem_size);
+    }
+    /* 创建边 */
     for (unsigned int i = 0; i < E; i++)
     {
-        G_Node *g_node = malloc(sizeof(struct _graph_node));
-        g_node->pos = es[i].head;
-        g_node->next = new_graph->adj[es[i].tail].next;
-        new_graph->adj[es[i].tail].next = g_node;
+        unsigned int arc_head_idx = es[i].arc_head_idx;
+        unsigned int arc_tail_idx = es[i].arc_tail_idx;
+        InsertHeadSList(&new_graph->adj_arr[arc_tail_idx].plain_slist, &arc_head_idx); // 尾到头
+        InsertHeadSList(&new_graph->adj_arr[arc_head_idx].plain_slist, &arc_tail_idx); // 头到尾
     }
     return new_graph;
+}
+
+void dfs_graph(const Graph graph)
+{
+    if (graph == NULL || graph->V == 0)
+        return;
+    bool *is_tranversed_arr = calloc_fatal(graph->V, sizeof(bool), "dfs_graph");
+    dfs_graph_recursive_helper(graph->adj_arr, 0, is_tranversed_arr);
+    free(is_tranversed_arr);
+}
+
+static void dfs_graph_recursive_helper(const Adj_List adj_arr, const unsigned int idx, bool is_tranversed_arr[])
+{
+    if (is_tranversed_arr[idx])
+        return;
+    printf("%d ", *(int *)adj_arr[idx].data);
+    is_tranversed_arr[idx] = true;
+    Node p = adj_arr[idx].plain_slist.head;
+    while (p)
+    {
+        unsigned int next_idx = *(unsigned int *)p->data;
+        dfs_graph_recursive_helper(adj_arr, next_idx, is_tranversed_arr);
+        p = p->next;
+    }
+}
+
+void bfs_graph(Graph graph)
+{
+    if (graph == NULL || graph->V == 0)
+        return;
+    bool *is_tranversed_arr = calloc_fatal(graph->V, sizeof(bool), "bfs_graph");
+    bfs_graph_recursive_helper(graph->adj_arr, graph->V, is_tranversed_arr);
+    free(is_tranversed_arr);
+}
+
+/*
+ * 两种bfs的方式：
+ * 1. 在入队前访问，并设置标志位
+ * 2. 在出队后访问，并设置标志位
+ * 
+ * 对于树，两者都行，因为树中没有“环”
+ * 
+ * 对于图，使用第2种方式的话。因为存在环路，可能会Enqueue同一个结点（此时该结点对应的标志位尚未被设置）
+ * 所以Queue的长度需要大于V（不知道最大是多少，可能这是一个神奇的数学问题）
+ * 
+ * 选择第1种方式可以避免这个问题
+ */
+static void bfs_graph_recursive_helper(const Adj_List adj_arr, const unsigned int V, bool is_tranversed_arr[])
+{
+    struct QueueRecord local_queue;
+    CreateLocalQueue(&local_queue, V, sizeof(unsigned int));
+    unsigned int zero_unsigned_int = 0;
+    printf("%d ", *(int *)adj_arr[0].data);
+    Enqueue(&zero_unsigned_int, &local_queue);
+    is_tranversed_arr[0] = true;
+
+    while (!IsEmpty(&local_queue))
+    {
+        unsigned int idx = *(unsigned int *)FrontAndDequeue(&local_queue);
+        for (Node p = adj_arr[idx].plain_slist.head; p; p = p->next)
+        {
+            unsigned int next_idx = *(unsigned int *)p->data;
+            if (!is_tranversed_arr[next_idx])
+            {
+                is_tranversed_arr[next_idx] = true;
+                printf("%d ", *(int *)adj_arr[next_idx].data);
+                Enqueue(p->data, &local_queue);
+            }
+        }
+    }
+
+    DisposeLocalQueue(&local_queue);
 }
