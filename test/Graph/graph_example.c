a2ec6a128a4f5f737e22647631f6c3e05bb935ef
Zhong_Ming <a18786717618@126.com>
2020-11-16 00:22:46 +0800

    1. 更复杂的例子 2. dfs、bfs例子

--- test/Graph/graph_example.c
+++ test/Graph/graph_example.c
@@ -1,9 +1,18 @@
 #include "graph.h"
+#include <stdio.h>
+
+#define V 6
+#define E 8
 
 int main(void)
 {
-    int nodes_data[5] = {0, 1, 2, 3, 4};
-    Edge es[4] = {{0, 1}, {1, 3}, {2, 3}, {3, 4}};
-    Graph *graph = create_graph_with_nodes(nodes_data, 5, es, 4);
+    int nodes_data[V] = {0, 1, 2, 3, 4, 5};
 
+    Edge es[E] = {{0, 1}, {0, 2}, {1, 3}, {1, 4}, {3, 5}, {5, 2}, {0, 3}, {4, 5}}; // {tail, head}
+    Graph graph = create_graph(nodes_data, V, sizeof(int), es, E);
+    dfs_graph(graph);
+    putchar('\n');
+    bfs_graph(graph);
+
+    return 0;
 }
